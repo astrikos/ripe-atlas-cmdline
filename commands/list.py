3e8f957498d445b6ba405ff3684a5def895e85fa
Andreas Strikos <astrikos@gmail.com>
2013-09-01 19:54:17 +0200

    add 2 more commands for list measurements and stopping them

---
+++ commands/list.py
@@ -0,0 +1,103 @@
+from optparse import make_option
+
+from . import AtlasCommand
+
+
+class Command(AtlasCommand):
+
+    help = (
+        'Fetch list of available UDMs. Capable of filtering and returning '
+        'specific only field'
+    )
+
+    options = [
+        make_option(
+            '-f', '--file', type='string', dest='key_file',
+            help='File containing api key. Specifing this option will fetch only UDMs belonging to you.'
+        ),
+        make_option(
+            '-t', '--flat', action='store_true', default=False,
+            dest='flat', help='Gives back a flat list of msm_ids.'
+        ),
+    ] 
+
+    url_path = '/api/v1/measurement/'
+
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        if not self.parser_options.help_text:
+            self._initialize()
+
+    def _initialize(self):
+        '''
+        Holds different stuff that we should do in init but we'd rather keep
+        __init__ clean.
+        '''
+        try:
+            f = open(self.parser_options.key_file, 'r')
+            self.key = f.read()
+        except:
+            print traceback.format_exc()
+            print 'Error while reading configuration'
+
+    def run(self):
+        self.stdin_options()
+        url = '%s%s' % (self.server, self.url_path)
+        if self.key:
+            url = '%s?key=%s' % (url, self.key)
+        url_query = ''
+        for f, v in self.filters.items():
+            if not url_query:  # first filter
+                url_query = '?%s=%s' % (f, v)
+            else:
+                url_query = '%s&%s=%s' % (url_query, f, v)
+
+        if self.fields:
+            if url_query:
+                url_query = '%s&fields=%s' % (url_query, ','.join(self.fields))
+            else:
+                url_query = '?fields=%s' % (','.join(self.fields))
+
+
+        url = '%s%s%s' % (self.server, self.url_path, url_query)
+         
+        results = self.http_get(url)
+        if not results:
+            return
+
+        full_results = not bool(self.parser_options.flat)
+        if self.parser_options.flat:
+            try:
+                print ','.join(map(str, [k['msm_id'] for k in results['objects']]))
+            except:
+                full_results = True
+
+        if full_results:
+            print 'The list of the fetched UDMs follows:\n%s' % (results)
+
+    def stdin_options(self):
+        '''
+        Collects from stdin filter options and desired fields.
+        '''
+        # filters part
+        self.filters = {}
+        if raw_input('Do you want any filter [y/n]:') == 'y':
+            while True:
+                option = raw_input('Specify filter:')
+                value = raw_input('Specify value:')
+                self.filters[option] = value
+                if raw_input('More [y/n]:') == 'y':
+                    continue
+                else:
+                    break
+        # fields part
+        self.fields = []
+        if raw_input('Do you want to specify any field (Otherwise you will get all) [y/n]:') == 'y':
+            while True:
+                field = raw_input('Specify field:')
+                self.fields.append(field)
+                if raw_input('More [y/n]:') == 'y':
+                    continue
+                else:
+                    break
---
+++ commands/stop.py
@@ -0,0 +1,78 @@
+import urllib2
+import traceback
+from optparse import make_option
+
+from . import AtlasCommand
+
+
+class Command(AtlasCommand):
+
+    # Global options from parent class
+    help = 'Stop a UDM.'
+    options = [
+        make_option(
+            '-f', '--file', type='string', dest='key_file',
+            help='File containing api key.'
+        ),
+        make_option(
+            '-i', '--id', type='string', dest='msm_ids',
+            help='UDM id(s). This can be multiple values separated by commas.'
+        ),
+    ]
+
+    # Local option for this class
+    url_path = '/api/v1/measurement/'
+
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        if not self.parser_options.help_text:
+            self._initialize()
+
+    def _initialize(self):
+        if not self.parser_options.msm_ids:
+            print 'You have to specify at least one measurement id.'
+            self.safe_options = False
+            return
+
+        self.msm_ids = self.parser_options.msm_ids.split(',')
+
+        if not self.parser_options.key_file:
+            print 'You have to specify the file that holds your api key.'
+            self.safe_options = False
+            return
+        try:
+            f = open(self.parser_options.key_file, 'r')
+            self.key = f.read().strip()
+        except:
+            print traceback.format_exc()
+            print 'Error while reading configuration'
+
+
+    def run(self):
+        for msm_id in self.msm_ids:
+            url = '%s%s%s/?key=%s' % (self.server, self.url_path, msm_id, self.key)
+            response = self.http_delete(url)
+            if response:
+                if response[0] / 100 == 2:
+                    print 'UMD: #%s was stopped successfully.' % msm_id
+                else:
+                    print 'UMD: #%s was not stopped successfully. Return with HTTP code: %d and msg: %s' % (
+                        msm_id, response[0], response[1]
+                    )
+
+    def http_delete(self, url):
+        '''
+        Fuction that implements a http delete give a url.
+        '''
+        req = urllib2.Request(url)
+        req.add_header('Content-Type', 'application/json')
+        req.add_header('Accept', 'application/json')
+        req.get_method = lambda: 'DELETE'
+        try:
+            response = urllib2.urlopen(req)
+        except:
+            print 'Problem with HTTP request (url: %s).' % url
+            print traceback.format_exc()
+            return False
+
+        return (response.code, response.msg)
