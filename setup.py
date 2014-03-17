306ae05c0c6abe134b027ae6cfa65738212584fb
Andreas Strikos <astrikos@gmail.com>
2014-03-17 22:57:27 +0100

    add package setup.py file

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+from atlascli import __version__
+
+install_requires = []
+tests_require = [
+    'nose',
+    'coverage',
+    'mock',
+    'jsonschema'
+]
+
+setup(
+    name='atlascli',
+    version=__version__,
+    description='Python command line tool for RIPE ATLAS API',
+    author="Andreas Strikos",
+    author_email="andreas@strikos.name",
+    packages=find_packages(
+        where='.',
+        exclude=('tests*', )
+    ),
+    install_requires=install_requires,
+    tests_require=tests_require,
+    test_suite="nose.collector",
+)
