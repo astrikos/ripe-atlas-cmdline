900bc6565af405290fce432b2dd710f81301c093
Andreas Strikos <astrikos@gmail.com>
2014-04-06 18:50:44 +0200

    change string to double quotes

--- atlascli/commands/__init__.py
+++ atlascli/commands/__init__.py
@@ -5,31 +5,31 @@ from optparse import OptionParser, make_option
 
 
 class AtlasCommand(object):
-    help = ''
+    help = ""
     options = []
 
     def __init__(self, sys_args):
-        self.server = 'https://atlas.ripe.net'
+        self.server = "https://atlas.ripe.net"
         basic_options = [
             make_option(
-                '--help_text', action='store_true', dest='help_text',
-                help='shows a small description of what the command is about'
+                "--help_text", action="store_true", dest="help_text",
+                help="shows a small description of what the command is about"
             ),
         ]
         fin_options = basic_options + self.options
         parser = OptionParser(
-            'usage: atlas_manage.py command [options]',
+            "usage: atlas_manage.py command [options]",
             option_list=fin_options
         )
 
-        (self.parser_options, args) = parser.parse_args(sys_args)
+        (self.parser_options, _) = parser.parse_args(sys_args)
         self.safe_options = True
         if self.parser_options.help_text:
             print self.help
 
     def run(self):
-        print('Available commands:')
-        for _, package_name, _ in pkgutil.iter_modules(['./commands']):
+        print "Available commands:"
+        for _, package_name, _ in pkgutil.iter_modules(["./commands"]):
             print package_name
 
     def http_get(self, url):
@@ -37,12 +37,12 @@ class AtlasCommand(object):
         Common function that implements a http get given a url
         """
         req = urllib2.Request(url)
-        req.add_header('Content-Type', 'application/json')
-        req.add_header('Accept', 'application/json')
+        req.add_header("Content-Type", "application/json")
+        req.add_header("Accept", "application/json")
         try:
             response = urllib2.urlopen(req)
-        except urllib2.HTTPError as e:
-            log = "HTTP ERROR %d: %s <%s>" % (e.code, e.msg, e.read())
+        except urllib2.HTTPError as exc:
+            log = "HTTP ERROR %d: %s <%s>" % (exc.code, exc.msg, exc.read())
             print log
             return False
         data = json.load(response)
